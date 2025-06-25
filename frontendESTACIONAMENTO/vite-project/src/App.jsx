22131cfe91257132b60fb5edc6ad660466a0d88e
LuizHAHAHA <luiz_gustavo-dias@estudante.sesisenai.org.br>
2025-06-25 19:57:28 -0300

    Add files via upload

--- frontendESTACIONAMENTO/vite-project/src/App.jsx
+++ frontendESTACIONAMENTO/vite-project/src/App.jsx
@@ -1,39 +1,34 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 import './App.css'
 
-// function App() {
-//   // Use State serve pra criar variaveis reativas, que atualizam na tela a cada mudança de valor
-//   // const [nomeEstado, funcaoQueMudaOEstado] = useState(valorInicial)
-//   let [count, setCount] = useState(5) // variavel com useState atualiza na tela a cada mudança de valor usando a função set
-//   let contador = 0 // variavel sem useState nao atualiza na tela quando seu valor muda
-//   let [tarefas, setTarefas] = useState([])
-//   let [descricao, setDescricao] = useState('Estudar CSS')
-//   // Padrão [estado, setEstado] = useState(valor)
-//   let [senai, setSenai] = useState('')
-//   let [nome, setNome] = useState('Ruan')
-  
-//   // Use Effect: serve para realizar ações associadas a um estado ou ao carregamento da página
-//   // useEffect(acao para executar, evento acionador)
-//   // useEffect(() => console.log('A página carregou'), []) // [] vai considerar como acionador, o carregamento da página (1 vez)
-//   // useEffect(() => console.log('O count mudou'), [count]) // [estado] vai considerar como acionador a mudança do estado (count, nesse exemplo) (executa algumas vezes)
-//   // useEffect(() => console.log('O count mudou')) // deixar vazio vai considerar como acionador a mudança de qualquer estado - não recomendo fazer isso (executa muitas vezes)
+function App() {
+  const [count, setCount] = useState(0)
 
-//   // Ao carregar a página, buscar dados de uma API
-//   useEffect(() => {
-//     chamarApi()
-//   }, []) // [] para executar só quando a pagina for iniciada
+  return (
+  <>
+    <header>
+      <title>Tela Login</title>
+    </header>
+      <div className="login-box">
+        <img src="src/assets/Senai-Sesi.png" className="logo" alt="Sesi logo"/>
+        <h2 className='tituloBox'>Login</h2>
+        <input type="text" placeholder='Nome Completo' id='nomeLog'/>
+        <br/>
+        <input type="text" placeholder='CPF' id='cpfLog'/>
+        <br/>
+        <input type="text" placeholder='Email' id='emailLog'/>
+        <br/>
+        <input type="Password" placeholder='Senha' id='senhaLog'/>
+        <br/>
+        <hr class="rounded"></hr>
+        <button>Entrar</button>
+        <br/>
+        <a href="pagina de registro" className='idLink'>Se não tem uma conta registre-se aqui</a>
+      </div>
+        
+  </>
+  )
+}
 
-//   async function chamarApi(){
-//     // Chamar uma API (conectar o frontend com o backend)
-//     // fetch => forma nativa do javascript para chamar API (realizar requisição HTTP)
-//     // => Nao depende de pacotes, pois é nativo 
-    
-//     const retorno = await axios.get('http://localhost:3000/tarefa') // se tiver API local rodando
-//     console.log('valor do retorno', retorno.data.tarefas)
-//     // Atualizar estado de tarefas
-//     setTarefas(retorno.data.tarefas)
-//   }
-// }
-
-// export default App
+export default App
--- frontendESTACIONAMENTO/vite-project/src/main.jsx
+++ frontendESTACIONAMENTO/vite-project/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import veiculos from './veiculos.jsx' 
-const App = veiculos
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
