596350f0bb48b669d01f021301dba295813b9375
LuizHAHAHA <luiz_gustavo-dias@estudante.sesisenai.org.br>
2025-07-01 21:20:43 -0300

    Delete backendESTACIONAMENTO directory

--- backendESTACIONAMENTO/rotas/usuario.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { registrarUsuario, autenticarUsuario } from '../controllers/usuario.js'
-const router = express.Router()
-
-router.post('/usuario', registrarUsuario)
-router.post('/login', autenticarUsuario)
-
-export { router }
--- backendESTACIONAMENTO/rotas/veiculos.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { registrarVeiculo } from '../controllers/veiculos.js'
-import { validarToken } from '../middlewares/usuario.js'
-
-const routerVeiculos = express.Router()
-
-routerVeiculos.post('/veiculos', validarToken, registrarVeiculo)
-
-export { routerVeiculos }
