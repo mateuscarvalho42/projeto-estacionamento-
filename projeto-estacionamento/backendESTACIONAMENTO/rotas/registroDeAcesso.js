2676cc2c1457958987adab1a21e5804197db0224
LuizHAHAHA <luiz_gustavo-dias@estudante.sesisenai.org.br>
2025-07-01 21:20:57 -0300

    Delete projeto-estacionamento directory

--- projeto-estacionamento/backendESTACIONAMENTO/rotas/registroDeAcesso.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { horarioEntrada, horarioSaida } from '../controllers/registroDeAcesso.js'
-
-const routerAcesso = express.Router()
-
-routerAcesso.post('/registroEntrada', horarioEntrada)
-routerAcesso.put('/registroSaida/:id', horarioSaida)
-
-export { routerAcesso }
